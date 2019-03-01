07863c1945b112e495aa6cd28ecc6277f8903b4d
Ian Hierl <ihierl@ku.edu>
2019-02-28 23:32:32 -0600

    quiz.php updated
    basic answer key to check user input from previous page

--- exercise2/quiz.php
+++ exercise2/quiz.php
@@ -4,5 +4,23 @@
   <head>
   </head>
   <body>
+<?php
+
+$answerKey = array(
+  "question1" => "a",
+  "question2" => "a",
+  "question3" => "a",
+  "question4" => "a",
+  "question5" => "a"
+);
+
+foreach($_POST as $key => $val) {
+  if($answerKey[$key] == $val) {
+    print("$key answered correctly. <br>\n");
+  } else {
+    print("$key answered incorrectly. <br>\n");
+  }
+}
+?>
   </body>
 </html>
